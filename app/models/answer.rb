c544391c80b63766cc153b8009bc5d8d40aadc35
Divagueame <martin.arce.teixeira@gmail.com>
2022-10-07 21:44:14 +0200

    Add answers tests

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   
+  validates :title, length: { minimum: 1 }
   validates :user_id, uniqueness: {
     scope: [ :question_id ]
   }
