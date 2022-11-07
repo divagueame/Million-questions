a5d3e2ab47cd848f305d2e91eed1020a9b9373e0
Martin Arce <martin.arce.teixeira@gmail.com>
2022-11-07 22:19:48 +0100

    Cleaning Stimulus

--- app/javascript/controllers/answer_card_controller.js
+++ app/javascript/controllers/answer_card_controller.js
@@ -8,57 +8,28 @@ export default class extends Controller {
   }
  
   connect() {
-    this.answerInputTarget.focus(); 
-    
-    // progress_bar_identifier
-    setTimeout(() => {
-      // this.element.classList.remove('opacity-0');
-      // this.element.classList.add('opacity-100')
-      // this.element.classList.add('bg-green-100')
-    }, 1800);
+    this.answerInputTarget.focus();  
   }
 
-  validate(e){
-    
-    if(!this.answerInputTarget.textLength){
-      e.preventDefault();
+  validate(e){ 
+    if(!this.answerInputTarget.value.length){
       this.answerInputTarget.classList.add('invalid-text-input');
-      
       setTimeout(() => {
         this.answerInputTarget.classList.remove('invalid-text-input')
       }, 750);
 
     } else {
-      e.preventDefault();
       
       document.getElementById("progress").progress.update()
 
       this.answerSubmitTarget.disabled = true;
       this.answerSubmitTarget.value = 'Next...'
-      this.answerSubmitTarget.classList.remove('bg-blue-600')
-      this.answerSubmitTarget.classList.remove('hover:bg-blue-600')
-      this.answerSubmitTarget.classList.add('bg-emerald-500')
       this.answerSubmitTarget.classList.add('hover:bg-emerald-400')
-      this.layoverTarget.classList.add('translate-y-0')
-      // this.questionTarget.classList.add('opacity-0');
+      this.answerSubmitTarget.classList.add('bg-emerald-500')
       
-      // this.answerInputTarget.classList.add('scale-x-100')
-      setTimeout(() => {
-        // this.answerInputTarget.classList.remove('scale-x-100')
-        // this.answerInputTarget.classList.add('scale-x-0')
-        // this.answerInputTarget.parent().classList.remove('w-full')
-        // this.answerInputTarget.parentElement.classList.add('w-0')
-        // this.answerInputTarget.classList.add('px-0')
-        // console.log(this.answerInputTarget.parentElement)
-      }, 500);
-
       setTimeout(() => {
         Turbo.navigator.submitForm(e.target)  
       }, 1000);
-      
-      
-      // e.target.requestSubmit(this.answerSubmitTarget)
-      // this.answerSubmitTarget.click()
       
     }
   }
