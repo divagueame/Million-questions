f95eee434429501f7bb27cdaf213436c1b0655bb
Martin Arce <martin.arce.teixeira@gmail.com>
2022-12-12 17:19:33 +0100

    Basic test for gae control

--- test/controllers/game_controller_test.rb
+++ test/controllers/game_controller_test.rb
@@ -1,32 +1,37 @@
-require "test_helper"
+require 'test_helper'
 
 class GameControllerTest < ActionDispatch::IntegrationTest
-
   setup do
     @user_game_ended = users(:one)
     @user_game_going = users(:two)
     @user_no_game = users(:three)
   end
 
-  test "should redirect to game report if user game is ended" do
+  test 'should redirect to game report if user game is ended.' do
     sign_in @user_game_ended
     get game_url
     assert_redirected_to game_report_path
   end
 
+  test 'should show game page if user game is not ended' do
+    sign_in @user_game_going
+
+    get game_url
+    assert_response :success
+  end
+  
 
   # test "should get index if game is going" do
-  #   sign_in @user_game_going 
+  #   sign_in @user_game_going
   #   get game_url
   #   assert_response :success
   # end
 
   # test "should create game if user no game" do
-  #   sign_in @user_no_game 
+  #   sign_in @user_no_game
   #   get game_url
   #   assert_response :success
   # end
-
 
   # test "should delete game and redirect to game" do
   #   sign_in @user_game_ended
@@ -37,16 +42,12 @@ class GameControllerTest < ActionDispatch::IntegrationTest
   #   assert_redirected_to game_url
   # end
 
-
-
   # test "should not delete game and redirect to game" do
   #   sign_in @user_game_going
-    
+
   #   assert_difference("Game.count", 0) do
   #     delete game_url
   #   end
   #   assert_redirected_to game_url
   # end
-
-
 end
